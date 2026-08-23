0e739f253bb2e878ccf76c83e887b991dd02ffef
Khusbu Jaiswal <khj98047@gmail.com>
2026-08-23 16:29:03 +0545

    coms

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -443,7 +443,7 @@ export function EducationCerts() {
 
               <div className="mt-auto pt-6">
                 <div className="border border-acid/30 bg-acid/5 px-4 py-3 font-mono text-xs text-acid">
-                  <span className="text-fog">gpa:Complated</span>{" "}
+                  <span className="text-fog">gpa:</span>{" "}
                   {education.gpa}
                 </div>
               </div>
